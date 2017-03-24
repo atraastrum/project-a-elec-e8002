dfad3e9b87dbd2e370ea9405a10f3b4e53bbf5d1
Boris <borys.pleynov@gmail.com>
2017-03-24 12:59:59 +0200

    Basic succesful measurment for 20 sec with step signal

--- gamry-600-reference/tests/PotentioStatControlTest/main.cpp
+++ gamry-600-reference/tests/PotentioStatControlTest/main.cpp
@@ -6,6 +6,15 @@
 
 #include "gamrypotentiostat.hpp"
 
+
+
+#include <atlcom.h>
+
+using namespace ATL;
+
+//#include <atlcom.h>
+//#include <atlctl.h>
+
 BOOL CtrlHandler( DWORD fdwCtrlType )
 {
   switch( fdwCtrlType )
@@ -20,12 +29,6 @@ BOOL CtrlHandler( DWORD fdwCtrlType )
   }
 }
 
-class CClient : public _IGamryDeviceListEvents {
-public:
-  CClient();
-  ~CClient();
-  virtual void OnDeviceListChanged() = 0;
-};
 
 int main(int argc, char *argv[])
 {
@@ -33,99 +36,180 @@ int main(int argc, char *argv[])
 
   CoInitialize(nullptr);
 
-#if 1
+
   CComPtr<IGamryDeviceList> spDeviceList;
   HRESULT hr = spDeviceList.CoCreateInstance(__uuidof(GamryDeviceList));
-  if(FAILED(hr))
+
+  if(FAILED(hr)) {
     std::cout << "Failed 1\n";
+    return -1;
+  }
 
-  std::cout << spDeviceList->Count() << std::endl;
+  CComPtr<IGamryPstat> spPstat;
+  hr = spPstat.CoCreateInstance(__uuidof(GamryPstat));
 
-  CComPtr<IConnectionPoint> spcp;
-  CComQIPtr<IConnectionPointContainer> spcpc(spDeviceList);
-  DWORD m_dwCookie;
 
-  hr = spcpc->FindConnectionPoint( __uuidof(_IGamryDeviceListEvents), &spcp);
-  if(FAILED(hr))
+  if(FAILED(hr)) {
     std::cout << "Failed 2\n";
-
- CClient cl{};
- //spcp->Advise(&cl, &m_dwCookie);
-
-#endif
-/*
-  try {
-
-      std::cout << spDeviceList->Count() << std::endl;
-  }catch(...) {
-    ;
+    return -1;
   }
 
-  CComPtr<_IGamryDeviceListEvents> pControl;
-  hr = spDeviceList->QueryInterface(IID_PPV_ARGS(&pControl));
-  if(FAILED(hr)){
-    std::cout << "asdasdasd";
-  }*/
+  CComPtr<IGamryDtaqChrono> spDtaqChrono;
+  hr = spDtaqChrono.CoCreateInstance(__uuidof(GamryDtaqChrono));
 
-#if 0
-  CComPtr<_IGamryDeviceListEvents> spDeviceList;
-  HRESULT hr = spDeviceList.CoCreateInstance(__uuidof(GamryDeviceList));
-  if(FAILED(hr))
-      std::cout << "Failed\n";
-#endif
-
-  // Instrument Interactions
-
-  //IGamryPstat* lpPstat = nullptr;
-
-  // CoCreateInstance Creates COM object and get interfaces from the obejct
-
-
-  //CoInitialize(nullptr);
-
-
-
-  /*
-  try {
-    Gamry::DeviceList dl;
-  }catch(Gamry::com_exception err) {
-    std::cout << "AAAAA\n";
+  if(FAILED(hr)) {
+    std::cout << "Failed 3\n";
+    return -1;
   }
-*/
-  //std::cout << dl.count() << std::endl;
-  //std::cout << dl.getLabelOfDeviceWithIdx(0) << std::endl;
-  //std::cout << dl.getSectionOfDeviceWithIdx(0) << std::endl;
+
+  CComPtr<IGamrySignalStep> spSignalStep;
+  hr = spSignalStep.CoCreateInstance(__uuidof(GamrySignalStep));
+
+  if(FAILED(hr)) {
+    std::cout << "Failed 4\n";
+    return -1;
+  }
 
 
+  long count = spDeviceList->Count();
+  if (count > 0) {
+    std::cout << "Device found\n" ;
 
+    std::string section;
+    long idx = 0;
+    BSTR sectionBSTR = nullptr;
+    if (idx >=0 && idx < count) {
+      SAFEARRAY* psa = nullptr;
+      psa = spDeviceList->EnumSections();
+      SafeArrayGetElement(psa, &idx, &sectionBSTR);
+      if (sectionBSTR) {
+        section = _bstr_t(sectionBSTR);
+      } else {
+        qDebug() << "Unable to get Label for idx " << idx << "\n";
+      }
+      SafeArrayDestroy(psa);
+    }
 
-  QObject::connect(&a, &QCoreApplication::aboutToQuit, [=]{
-    //std::cout << "Out of Application" << std::endl;
-  });
+    std::cout<< section << std::endl;
+    spPstat->Init(sectionBSTR);
+    BSTR bstrSerial = spPstat->SerialNo();
 
-  SetConsoleCtrlHandler( (PHANDLER_ROUTINE) CtrlHandler, TRUE );
+    if (bstrSerial)
+      std::cout << _bstr_t(bstrSerial) << std::endl;
 
-  int r = a.exec();
+    std::cout << "Initializing  IGamryDtaqChrono\n";
+    spDtaqChrono->Init(spPstat, ChronoAmp);
+
+    std::cout << "Initializing  IGamrySignalStep\n";
+    spSignalStep->Init(spPstat, 0.5f, 5.0f, -0.1f, 5.0f, 0.01f, PstatMode);
+
+    std::cout << "Opening potentiostat\n";
+    spPstat->Open();
+
+    std::cout << "Setting parameters of Pstat\n";
+    spPstat->SetCell (CellOff);
+    spPstat->SetAnalogOut (0.0);
+    spPstat->SetVoltage (0.0);
+
+    std::cout << "Setting signal\n";
+    IGamrySignal* lpSignalBase = NULL;
+    spSignalStep->QueryInterface(&lpSignalBase);
+    spPstat->SetSignal(lpSignalBase);
+
+    std::cout << "Pstat is initializing signal\n";
+    spPstat->InitSignal();
+
+    std::cout << "Pstat turning cell on..." << std::endl;
+    spPstat->SetCell(CellOn);
+
+    // Run the Experiment
+    std::cout << "DtaqChrono start running\n";
+    spDtaqChrono->Run(VARIANT_TRUE);
+
+    // Output Data
+    VARIANT Voltage_data;
+    VARIANT Current_data;
+
+    long index_v[2];
+    long index_i[2];
+    long PointCount = 0;
+    long CurrentPoint = 0;
+    const long PointsToCook = 256;
+
+    const int poll_delay = 300;
+    const int delay_interval = 100;
+
+    SAFEARRAY * psa;
+
+    std::cout << "Before 100 sec sleep\n";
+    Sleep(100000);
+    std::cout << "After 100 sec sleep\n";
+    CurrentPoint = 0;
+    PointCount = PointsToCook;
+    spDtaqChrono->Cook(&PointCount, &psa);
+
+    std::cout << "PointCount is " << PointCount << std::endl;
+
+    // Always "Cook" until routine returns no data.
+    while (PointCount > 0)
+    {
+      // Loop through and display each point
+      for (int i = 0; i < PointCount; i++)
+      {
+        CurrentPoint++;
+
+        // Indexes are specific locations based on Dtaq.  See documentation for details.
+        // SafeArray indexing is reversed from normal conventions.
+
+        index_v[0] = 1;
+        index_v[1] = i;
+
+        index_i[0] = 3;
+        index_i[1] = i;
+
+        SafeArrayGetElement(psa, index_v, &Voltage_data);
+        SafeArrayGetElement(psa, index_i, &Current_data);
+
+        std::cout << Voltage_data.fltVal << "  " << Current_data.fltVal << "\n";
+      }
+
+      SafeArrayDestroy(psa);
+
+      // Cook and loop
+      PointCount = PointsToCook;
+      spDtaqChrono->Cook(&PointCount, &psa);
+    }
+
+    SafeArrayDestroy(psa);
+    std::cout << std::endl;
+    std::cout << "Experiment complete." << std::endl;
+
+    std::cout << "DtaqChrono stops running\n";
+    spDtaqChrono->Stop();
+
+    std::cout << "Pstat turning Cell Of\n";
+    spPstat->SetCell(CellOff);
+    std::cout << "Pstat closing\n";
+    spPstat->Close(VARIANT_TRUE);
+  }
 
 
   CoUninitialize();
-  return r;
+
+  //QObject::connect(&a, &QCoreApplication::aboutToQuit, [=]{
+  //  CoUninitialize();
+  //});
+
+  //SetConsoleCtrlHandler( (PHANDLER_ROUTINE) CtrlHandler, TRUE );
+
+  //int r = a.exec();
+
+
+
+  return 0;
 }
 
 
 
 
 
-CClient::CClient()
-{
-
-}
-
-CClient::~CClient()
-{
-
-}
-
-void CClient::OnDeviceListChanged() {
-  std::cout << "AAAAA\n";
-}
