3132c2e103e8cb079d201063c4efee845f39402b
Borys Plyenkov <borys.plyenkov@gmail.com>
2017-03-25 00:59:18 +0200

    Implementation of basic methods to control Experiement

--- gamry-600-reference/src/GamryChronoAmperometrySimpleUI/potentiostat.cpp
+++ gamry-600-reference/src/GamryChronoAmperometrySimpleUI/potentiostat.cpp
@@ -37,4 +37,70 @@ namespace Gamry {
   {
     return spDeviceList->Count();
   }
+
+  void Potentiostat::setStepSignal(float vInit, float tInit, float vFinal, float tFinal, float sampleRate)
+  {
+    qDebug() << "Initializing  IGamrySignalStep\n";
+    spSignalStep->Init(spPstat, vInit, tInit, vFinal, tFinal, sampleRate, PstatMode);
+    IGamrySignal* lpSignalBase = nullptr;
+    spSignalStep->QueryInterface(&lpSignalBase);
+    spPstat->SetSignal(lpSignalBase);
+  }
+
+  void Potentiostat::init()
+  {
+    if (deviceCount() > 0)
+    {
+      std::string section;
+      BSTR sectionBSTR = nullptr;
+      long idx = 0;
+      SAFEARRAY* psa = nullptr;
+      psa = spDeviceList->EnumSections();
+      SafeArrayGetElement(psa, &idx, &sectionBSTR);
+      if (sectionBSTR) {
+          section = _bstr_t(sectionBSTR);
+      } else {
+        qDebug() << "Unable to get Label for idx " << idx << "\n";
+      }
+      SafeArrayDestroy(psa);
+
+      spPstat->Init(sectionBSTR);
+
+
+      qDebug() << "Initializing  IGamryDtaqChrono\n";
+      spDtaqChrono->Init(spPstat, ChronoAmp);
+    }
+  }
+
+  void Potentiostat::open()
+  {
+    qDebug() << "Opening potentiostat\n";
+    spPstat->Open();
+
+    qDebug() << "Switching cell switch off\n";
+    spPstat->SetCell (CellOff);
+
+    spPstat->SetCtrlMode (PstatMode);
+    spPstat->SetIEStability (StabilityNorm);
+
+    //spPstat->SetIConvention (Cathodic);
+
+    spPstat->SetAnalogOut (0.0);
+    spPstat->SetVoltage (0.0);
+  }
+
+  void Potentiostat::start()
+  {
+    qDebug() << "Pstat is initializing signal\n";
+    spPstat->InitSignal();
+
+    qDebug() << "Pstat turning cell on...\n";
+    spPstat->SetCell(CellOn);
+
+    // Run the Experiment
+    qDebug() << "DtaqChrono start running\n";
+    spDtaqChrono->Run(VARIANT_TRUE);
+  }
+
+
 }
--- gamry-600-reference/src/GamryChronoAmperometrySimpleUI/potentiostat.hpp
+++ gamry-600-reference/src/GamryChronoAmperometrySimpleUI/potentiostat.hpp
@@ -12,7 +12,25 @@ namespace Gamry {
     explicit Potentiostat();
     ~Potentiostat();
 
+    // Returns the count of device connected. Call Count of GAMRYCOMLib::IGamryDeviceList
     long deviceCount();
+
+    // Initializes GAMRYCOMLib::IGamryPstat and GAMRYCOMLib::IGamryDtaqChrono
+    void init();
+
+    // Initilize GAMRYCOMLib::IGamryStepSignal and set the signal with GAMRYCOMLib::IGamryPstat::SetSignal.
+    // Think and Test what happens if setStepSignal is called before init
+    void setStepSignal(float vInit, float tInit, float vFinal, float tFinal, float sampleRate);
+
+    // Opens potentiostat and sets the cell to off position
+    // When the the cell is off, the counter electrode is disconnected and no cell current can
+    // flow. When the cell switch is in the on position, cell current can flow and counter
+    // electrode is connected. This method also set some parameters by calling methods of
+    // GAMRYCOMLib::IGamryPstat.
+    void open();
+
+    void start();
+
   private:
     CComPtr<IGamryDeviceList> spDeviceList;
     CComPtr<IGamryPstat> spPstat;
