6faf10ef36c5db83fe86675bccf9b350f8ab131a
TracyQin326 <qianqian.qin@aalto.fi>
2017-03-25 03:15:35 +0200

    Add files via upload

---
+++ user-interface/UI-with-Qt5/mainwindow.cpp
@@ -0,0 +1,20 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent)
+
+{
+    ui.setupUi(this);
+    this->setWindowIcon(QIcon(":/new/prefix1/ico"));
+    currenttime = new QLabel(this);
+    currenttime->setGeometry(QRect(5,380,220,20));
+    timer = new QTimer;
+    connect(timer,SIGNAL(timeout()),this,SLOT(timerTime()));
+    timer->start(1000);
+}
+void MainWindow::timerTime()
+{
+QDateTime sysTime = QDateTime::currentDateTime();
+currenttime->setText(sysTime.toString("yyyy-MM-dd-hh:mm:ss"));
+}
