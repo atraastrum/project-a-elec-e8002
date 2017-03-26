8b44d509d5f00a7bf533d5531ac68517982eddc3
Borys Plyenkov <borys.plyenkov@gmail.com>
2017-03-26 22:24:09 +0300

    TestPontetiostatClass will write data to file and to standard output

--- gamry-600-reference/src/TestPontentiostatClass/main.cpp
+++ gamry-600-reference/src/TestPontentiostatClass/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "../GamryChronoAmperometrySimpleUI/potentiostat.hpp"
 
 int main()
@@ -14,12 +15,44 @@ int main()
 
 
     size_t points_accuired = 0;
+    std::fstream ofile;
+
+    // CSV file format is used to store tabular data
+    ofile.open("data.csv");
     do {
       Sleep(5000);
       std::vector<Gamry::CookInformationPoint> data = pStat.pullDataItems(100);
       points_accuired = data.size();
+      for(int i = 0; i < data.size(); ++i)
+      {
+        Gamry::CookInformationPoint& item = data[i];
+        ofile << item.Time << ';'
+              << item.Im << ';'
+              << item.Arch << ';'
+              << item.IERange << ';'
+              << item.Overload << ';'
+              << item.Q << ';'
+              << item.StopTest << ';'
+              << item.Vf << ';'
+              << item.Vsig << ';'
+              << item.Vu << ';'
+              << std::endl;
+        std::cout << item.Time << ';'
+              << item.Im << ';'
+              << item.Arch << ';'
+              << item.IERange << ';'
+              << item.Overload << ';'
+              << item.Q << ';'
+              << item.StopTest << ';'
+              << item.Vf << ';'
+              << item.Vsig << ';'
+              << item.Vu << ';'
+              << std::endl;
+      }
+
     }while(points_accuired > 0);
 
+    ofile.close();
     pStat.close();
   } else
     std::cout << "Unable to find the device, closing this application\n";
