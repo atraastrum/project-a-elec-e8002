ab8baf4d2d6a2c2be8673073b43a412479d630f6
TracyQin326 <qianqian.qin@aalto.fi>
2017-04-25 16:55:49 +0300

    limitating the polling interval input as an float

--- user-interface/ExperimentControl/dialog.cpp
+++ user-interface/ExperimentControl/dialog.cpp
@@ -8,6 +8,7 @@ Dialog::Dialog(QWidget *parent) :
     ui->setupUi(this);
     QRegExp regExp("^[1-9][0-9]{1,8}$");
     ui->SampleRate->setValidator(new QRegExpValidator(regExp, this));
+    ui->PollingInterval->setValidator(new QDoubleValidator(0,1000,2,this));
 
 }
 
